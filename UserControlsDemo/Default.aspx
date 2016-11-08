d0d4341d9812b54b10ce219c945886b7d9236a26
Kevin Ma <kbma.kevin@gmail.com>
2016-11-08 08:31:04 -0500

    added demo on how to add/remove controls programmatically

---
+++ UserControlsDemo/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="UserControlsDemo.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="NumIngredientsDropDownList" runat="server" OnSelectedIndexChanged="NumIngredientsDropDownList_SelectedIndexChanged">
+        </asp:DropDownList>
+        <asp:Button Text="Generate" runat="server" ID="GenerateButton" OnClick="GenerateButton_Click" />
+    <asp:panel runat="server" ID="IngredientsPanel">
+        <asp:Label Text="N/A" runat="server" />
+    </asp:panel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ UserControlsDemo/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UserControlsDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Populate the drop down list when the page loads
+                DropDownList ddl = (DropDownList)Page.FindControl("NumIngredientsDropDownList");
+                for (int i = 1; i <= 15; i++)
+                {
+                    ListItem newListItem = new ListItem();
+                    Label l = new Label();
+                    newListItem.Value = i.ToString();
+
+                    ddl.Items.Add(newListItem);
+                }
+            }
+        }
+
+        protected void NumIngredientsDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Panel tempPanel = (Panel)Page.FindControl("IngredientsPanel");
+            tempPanel.Controls.Clear();
+            DropDownList ddl = (DropDownList)Page.FindControl("NumIngredientsDropDownList");
+            TextBox tb;
+            for (int i = 1; i <= int.Parse(ddl.SelectedValue); i++)
+            {
+                tb = new TextBox();
+                tb.ID = "IngredientTextBox" + i;
+                tempPanel.Controls.Add(tb);
+            }
+        }
+
+        protected void GenerateButton_Click(object sender, EventArgs e)
+        {
+            Panel tempPanel = (Panel)Page.FindControl("IngredientsPanel");
+            tempPanel.Controls.Clear();
+            DropDownList ddl = (DropDownList)Page.FindControl("NumIngredientsDropDownList");
+            TextBox tb;
+            int selVal = int.Parse(ddl.SelectedValue);
+            for (int i = 1; i <= selVal; i++)
+            {
+                tb = new TextBox();
+                tb.ID = "IngredientTextBox" + i;
+                tempPanel.Controls.Add(tb);
+                tempPanel.Controls.Add(new LiteralControl("</br>"));
+            }
+        }
+    }
+}
